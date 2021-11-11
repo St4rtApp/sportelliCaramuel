3e54a4e9f03629419907cf4e555a420f5cd1df19
unknownMatt21 <perfumo.m10@gmail.com>
2021-11-11 13:47:38 +0100

    register button (final part)

--- docs/controller.php
+++ docs/controller.php
@@ -12,6 +12,8 @@ if(isset($_POST["register"])){
     $mail = $connessione->mysqli_real_escape_string($_POST["email"]);
     $pswd = $connessione->mysqli_real_escape_string($_POST["password"]);
     $cpswd = $connessione->mysqli_real_escape_string($_POST["cpassword"]);
+    $anno = $connessione->mysqli_real_escape_string($_POST["anno"]);
+    $corso = $connessione->mysqli_real_escape_string($_POST["corso"]);
 
     //verifico la password
 
@@ -27,13 +29,59 @@ if(isset($_POST["register"])){
             $errors['email']= "La mail inserita esiste già";
         }
     }else{
-        $errors["query"] ="query error";
+        $errors['db-error']="errore del database";
     }
+
+    //genero verification code per la mail
 
     if(count($errors) === 0){
+        $encpass= password_hash($pswd, PASSWORD_BCRYPT);
+        $code = rand(999999, 111111);
+        $status='notverified';
+        $query="INSERT INTO users (name, email, anno, corso, password, code, status)
+                values('$name','$email','$anno', '$corso', '$encpass', '$code', '$status')";
+
+        //invio della mail
+
+        if($data_check = $connessione->query($query)){
+           $subject = "Email Verification Code";
+           $message = "codice di verifica: $code";
+           $sender = "From: mail@caramuelroncalli.it";
+           if(mail($mail,$subject,$message,$sender)){
+               $info="Ti abbiamo mandato via email il codice di verifica";
+               $_SESSION['info'] = $info;
+               $_SESSION['email'] = $email;
+               $_SESSION['password'] = $pswd;
+               header('location: otp.php');
+               exit();
+           }else{
+               $errors['otp-error'] = "errore nell'invio del codice di verifica";
+           } 
+        }else{
+            $errors['db-error']="errore del database";
+        }
         
+
     }
 
+}
+
+
+
+
+//se l'utente clicca il bottone per la verifica della mail
+
+if(isset($_POST['check'])){
+    $_SESSION['info']="";
+    $otp_code= $connessione->mysqli_real_escape_string($_POST['otp']);
+    $check_code= "SELECT * FROM users WHERE code= '$otp_code'";
+    if($connessione->query($check_code)){
+        if($connessione->num_rows()>0){
+            
+        }
+    }else{
+        $errors['db-error']="errore del database";
+    }
 }
 
 
