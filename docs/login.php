3cf85e85b447ae512b3f50bdf2485c82fce993cd
Luxi99 <63002758+Luxi99@users.noreply.github.com>
2021-12-02 23:41:09 +0100

    Log in responsive

--- docs/login.php
+++ docs/login.php
@@ -5,13 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <link  href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet">
-        <link rel="stylesheet" href="test/style.css">
+        <link rel="stylesheet" href="style/login.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
 
     <body style="background-image: url('IMGs/study.jpg')"class="overflow-hidden">
 
-        <div id="error_popup" style="width:500px;position:relative;top:115px" class="hidden flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 ">
+        <div id="error_popup" style="width:500px;" class="hidden flex w-full max-w-sm mx-auto overflow-hidden relative top-36 bg-white rounded-lg shadow-md dark:bg-gray-800 ">
           <div class="flex items-center justify-center w-12 bg-red-500">
               <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
                   <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z"/>
@@ -25,33 +25,32 @@
           </div>
         </div>
 
-        <div style="width:500px;position:relative;top:125px" class="mx-auto shadow-lg pt-6 pb-10 bg-white rounded page-centered">
-            <h1 id="titolo" class="text-center text-xl">Login</h1>
+        <div id="form_container" class="mx-auto relative sm:top-40 shadow-lg pt-6 pb-10 bg-white rounded page-centered">
+            <h1 id="titolo" class="text-center sm:text-xl text-2xl">Login</h1>
             <div class="pt-8">
                 <form method="POST" id="login_form" class="mx-10">
                     <div class="relative flex w-full flex-wrap items-stretch mb-3">
-                        <span class="flex items-center z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base justify-center w-8 pl-2 py-1">
+                        <span class="flex items-center z-10 h-full leading-snug font-normal absolute text-center sm:text-base text-2xl text-gray-400 absolute bg-transparent rounded text-base justify-center w-8 pl-2 py-1">
                             <i class="fas fa-user"></i>
                         </span>
 
-                        <input name="email" type="text" placeholder="mail@esempio.com" class=" px-2 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white text-md border-b-2 border-gray-400 focus:rounded-xl focus:ring w-full pl-10"/>
+                        <input name="email" id="email" type="text" placeholder="mail@esempio.com" class="px-2 sm:py-3 py-4 sm:text-base text-2xl placeholder-gray-400 text-gray-600 relative bg-white bg-white text-md border-b-3 sm:border-b-2 border-gray-400 w-full pl-10"/>
                     </div>
 
                     <div class="relative flex w-full flex-wrap items-stretch">
-                        <span class="flex z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-2 py-1">
+                        <span class="flex z-10 h-full leading-snug font-normal absolute text-center sm:text-base text-2xl text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-2 py-1">
                             <i class="fas fa-lock"></i>
                         </span>
 
-                        <input name="password" id="pswd" type="password" placeholder="Password" class=" px-2 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white text-md border-b-2 border-gray-400 focus:rounded-xl focus:ring w-full pl-10"/>
+                        <input name="password" id="pswd" type="password" placeholder="Password" class=" px-2 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white sm:text-base text-2xl border-b-3 sm:border-b-2 border-gray-400 w-full pl-10"/>
 
-                        <span class="flex z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-2 py-1">
+                        <span class="flex z-10 h-full leading-snug font-normal absolute text-center sm:text-base text-2xl text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-2 py-1">
                             <i id="eye" class="fas fa-eye" onclick="showPassword()"></i>
                         </span>
                     </div>
-
-                    <div class="pt-4">
-                        <input id="submit_btn" name="login" type="submit" value="login" class="cursor-pointer bg-gradient-to-r from-green-400 to-blue-500 text-white font-bold uppercase text-sm px-4 py-1 rounded-full shadow hover:shadow-md hover:bg-blue-800 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
-                        <a id="dimenticanza" href="#" class="bg-gradient-to-r from-blue-500 to-blue-800 text-white active:bg-purple-600 font-bold uppercase text-sm px-4 py-1 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">Dimenticata?</a>
+                    <div class="sm:pt-4 pt-12">
+                        <input id="submit_btn" name="login" value="login" type="submit" class="cursor-pointer bg-gradient-to-r from-green-400 to-blue-500 text-white font-bold uppercase sm:text-sm text-lg sm:px-4 px-7 sm:py-1 py-2 sm:rounded-full rounded-lg shadow hover:shadow-md hover:bg-blue-800 outline-none focus:outline-none ease-linear transition-all duration-150">
+                        <a id="dimenticanza" href="new-pswd.php" class="bg-gradient-to-r from-blue-500 to-blue-800 text-white active:bg-purple-600 font-bold uppercase ml-1 sm:text-sm text-lg sm:px-4 px-7 sm:py-1 py-2 sm:rounded-full rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">Dimenticata?</a>
                     </div>
                 </form>
             </div>
@@ -62,23 +61,34 @@
               document.getElementById("error_popup").classList.remove("hidden");
             });
 
-            $("#submit_btn").click(function(){
-              $.ajax({
-                type: "POST",
-                url: "controller.php",
-                datatype: "html",
-                data: dataString,
+            function do_login(){
+              var email=$("#email").val();
+              var pass=$("#pswd").val();
+              if(email!="" && pass!=""){
+                $.ajax({
+                  type: "POST",
+                  url: "controller.php",
+                  datatype: "html",
+                  data:{
+                    do_login:"do_login",
+                    email:email,
+                    password:pass
+                  },
 
-                success: function(data) {
-                  var errori = $.parseJSON(data);
-                  if(errori.email!="" || errori.dberror!=""){
-                    document.getElementById("error_popup").classList.remove("hidden");
-                    document.getElementById("error_log").innerHTML=errori.email + errori.dberror;
+                  success: function(data) {
+                    if(data != ""){
+                      $("error_popup").removeClass("hidden");
+                    }
+                  },
+                  error: function() {
+                    alert("Problema di comunicazione con il server!");
                   }
-                },
-                error: function() {
-                  alert("Problema di comunicazione con il server!");
-                }
+              }
+
+            }
+
+            $("#submit_btn").click(function(){
+
               });
             });
 
