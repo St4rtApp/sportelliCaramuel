cdcf63ac7762714d6a69f33f5d3638e1ea55d25f
Mattia Perfumo <perfumo.m10@gmail.com>
2022-02-02 12:19:57 +0100

    adding instructions and pages

---
+++ docs/forgot-pswd.php
@@ -0,0 +1,1 @@
+<!-- pagina che richiede la mail dove si ha dimenticato la password e fa una POST a 'forgot-pswd'  -->
--- docs/new-pswd.php
+++ docs/new-pswd.php
@@ -1,1 +1,1 @@
-<?php?>
+<!-- pagina che richiede la nuova password e conferma password e fa una POST a 'change-pswd'  -->
